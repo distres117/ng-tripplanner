fb9681f5209ee770c30391eef2adf7358396036f
Ontima Chung <ontimachung@Ontimas-MacBook-Pro.local>
2016-03-13 15:48:52 -0400

    api

---
+++ routes/api/days.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var models = require("../../db");
+
+var Day = models.models.Day;
+
+router.get('/', function(req, res, next) {
+	Day.find()
+	.then(function(days){
+		res.json(days);
+	})
+});
+
+router.get('/:day', function(req, res, next) {
+	Day.findOne({currentDay:req.params.day})
+	.then(function(day){
+		res.json(day);
+	})
+});
+
+router.post('/', function(req, res, next) {
+	Day.count()
+	.then(function(count) {
+		var newDay = new Day({
+			currentDay: count+1 
+		})	
+		return newDay.save();
+	})
+	.then(function(day) {
+		res.json(day);
+	})
+});
+
+module.exports = router;
